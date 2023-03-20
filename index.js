543d447e0f76ef9e74c22af96a20a597cb34925b
LiljayZZ <ayanbodejohnadeboye@gmail.com>
2023-03-20 10:47:23 +0100

    change Readme

---
+++ index.js
@@ -0,0 +1,102 @@
+let firstName = "John";
+let lastName = "Ayanbode";
+let country = "Nigeria";
+let city = "Abuja";
+let age = 40;
+let isMarried = "Single";
+let year = 2023;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+
+console.log('10' == 10)
+console.log(9.8 == 10)
+
+let number = 3;
+number > 2
+? console.log(`${number} is a positve number`)
+: console.log(`${number} is a negative number`);
+
+let isDriving = true
+isDriving
+? console.log("You need to turn on the engine.")
+: console.log("No need to turn on the engine.");
+
+let isClean = true
+isClean
+? console.log("You need to wash the cloths.")
+: console.log("No need to wash the cloths.");
+
+let num = 5
+num < 3
+? console.log(`${num} is a positve number`)
+: console.log(`${num} is a negative number`);
+
+let isWatching = false
+isWatching
+? console.log("You need to turn on the TV.")
+: console.log("No need to turn on the TV.");
+
+let isDancing = false
+isDancing
+? console.log("You need to move your body.")
+: console.log("No need to move your body.")
+
+console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 !== 4);
+console.log(4 != '4');
+console.log(4 == '4');
+console.log(4 === '4');
+console.log("python".length != "jargon".length);
+
+const check = 4 > 3 && 10 < 12
+console.log(check)
+
+const checks = 4 > 3 && 10 > 12
+console.log(checks)
+
+const checker = 4 > 3 || 10 < 12
+console.log(checker)
+
+const checkers = 4 > 3 || 10 > 12
+console.log(checkers)
+
+const checkz = !(4 > 3)
+console.log(checkz)
+
+const come = !(4 < 3)
+console.log(come)
+
+const go = !(false)
+console.log(go)
+
+const John = !(4 > 3 && 10 < 12)
+console.log(John)
+
+const Mike = !(4 > 3 && 10 > 12)
+console.log(Mike)
+
+const Jane = !(4 === '4')
+console.log(Jane)
+
+const now = new Date();
+console.log(now);
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getTime());
+
